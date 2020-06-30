c63a2a7558129ca26fd27ab352727a314596fb20
张艳涛e <zhangyte@fotile.com>
2020-06-30 12:56:19 +0800

    新建提交 20200630

---
+++ test.py
@@ -0,0 +1,12 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import time
+import sys
+sys.path.append("..") # 为了导入上层目录的d2lzh_pytorch
+import d2lzh_pytorch as d2l
+
+
+mnist_train = torchvision.datasets.FashionMNIST(root='~/Datasets/FashionMNIST', train=True, download=True, transform=transforms.ToTensor())
+mnist_test = torchvision.datasets.FashionMNIST(root='~/Datasets/FashionMNIST', train=False, download=True, transform=transforms.ToTensor())
